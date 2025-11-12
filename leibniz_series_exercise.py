06b5e38623651f2c2821db6afdf0fac4f1916ff3
mohamedjbareen2003-design <mohamed.jbareen2003@gmail.com>
2025-11-12 21:22:00 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -3,6 +3,7 @@ def approximate_pi(n_terms):
     for i in range(n_terms):
         leibniz_series.append((-1)**i / (2*i + 1))
     sum_numbers = sum(leibniz_series)
+    
     approximate_pi = 4*(sum_numbers)
     return approximate_pi
-    pass # replace pass with your code
+
